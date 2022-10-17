9df108965c4a9bd891bad3d99cb3245eb0bab298
gnsdp99 <rladpgns99@gmail.com>
2022-10-18 02:50:09 +0900

    22.10.18

--- ocr/pill_api.py
+++ ocr/pill_api.py
@@ -2,19 +2,24 @@ import requests
 import json
 import pandas as pd
 
-# 약 상세정보를 csv파일로 저장하는 코드
-
 url = 'http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList'
-params ={'serviceKey' : 'PvrIUvAktOxd6AOTm2RPSs8ovw0vx517mITghrvn8+qD/IHJZQd4j+mjnD9nrT4/arMuTl44EGSvLLym15eBRQ==', 'pageNo' : '1', 'numOfRows' : '1', 'type' : 'json' }
-# params ={'serviceKey' : 'PvrIUvAktOxd6AOTm2RPSs8ovw0vx517mITghrvn8+qD/IHJZQd4j+mjnD9nrT4/arMuTl44EGSvLLym15eBRQ==', 'itemName' : '가나다', 'type' : 'json' }
-response = requests.get(url, params=params)
-contents = response.text
-print(contents)
-# pill_json = json.loads(contents)
+params ={'serviceKey' : 'PvrIUvAktOxd6AOTm2RPSs8ovw0vx517mITghrvn8+qD/IHJZQd4j+mjnD9nrT4/arMuTl44EGSvLLym15eBRQ==', 'pageNo' : '1', 'numOfRows' : '100', 'type' : 'json' }
 
-# items_dict = pill_json["body"]["items"] # 리스트로 감싼 dict
+def call_pill_api(): # 공공데이터포털 api 호출
+    response = requests.get(url, params=params)
+    return response.text # json file
 
-# df = pd.DataFrame(items_dict)
+def preprocess(json_file): # json파일 전처리
+    pill_json = json.loads(json_file) # json Object
+    items_dict = pill_json["body"]["items"] # 리스트로 감싼 dict
+    df = pd.DataFrame(items_dict)
+    new_df = df.drop(['itemSeq', 'openDe', 'updateDe', 'itemImage'], axis=1) # 필요없는 col 제거
+    return new_df
 
-# df.to_csv("pill.csv",  index=False)
+def save_csv(df): # csv파일로 저장
+    df.to_csv("pill.csv",  index=False)
 
+if __name__ == '__main__': # import 시에는 실행되지 않음
+    pill_json = call_pill_api()
+    df = preprocess(pill_json)
+    save_csv(df)
--- ocr/test.py
+++ ocr/test.py
@@ -1,19 +1,3 @@
-import json
-import pandas as pd
+import clova_ocr_api as ocr
 
-pill_df = pd.read_csv('pill.csv')
-
-text = '{"version":"V2","requestId":"cb29c07d-0ded-4873-9527-75f2e806faeb","timestamp":1665583554716,"images":[{"uid":"34681fe1d66b4a388a2489c8b704e6bf","name":"demo","inferResult":"SUCCESS","message":"SUCCESS","validationResult":{"result":"NO_REQUESTED"},"convertedImageInfo":{"width":1536,"height":864,"pageIndex":0,"longImage":false},"fields":[{"valueType":"ALL","boundingPoly":{"vertices":[{"x":4.0,"y":5.0},{"x":64.0,"y":5.0},{"x":64.0,"y":27.0},{"x":4.0,"y":27.0}]},"inferText":"파일(F)","inferConfidence":1.0,"type":"NORMAL","lineBreak":false},{"valueType":"ALL","boundingPoly":{"vertices":[{"x":74.0,"y":5.0},{"x":133.0,"y":5.0},{"x":133.0,"y":29.0},{"x":74.0,"y":29.0}]},"inferText":"편집(E)","inferConfidence":1.0,"type":"NORMAL","lineBreak":false},{"valueType":"ALL","boundingPoly":{"vertices":[{"x":144.0,"y":5.0},{"x":207.0,"y":5.0},{"x":207.0,"y":29.0},{"x":144.0,"y":29.0}]},"inferText":"서식(O)","inferConfidence":0.981,"type":"NORMAL","lineBreak":false},{"valueType":"ALL","boundingPoly":{"vertices":[{"x":218.0,"y":5.0},{"x":277.0,"y":5.0},{"x":277.0,"y":29.0},{"x":218.0,"y":29.0}]},"inferText":"보기(V)","inferConfidence":0.9999,"type":"NORMAL","lineBreak":false},{"valueType":"ALL","boundingPoly":{"vertices":[{"x":288.0,"y":5.0},{"x":369.0,"y":5.0},{"x":369.0,"y":27.0},{"x":288.0,"y":27.0}]},"inferText":"도움말(H)","inferConfidence":0.9999,"type":"NORMAL","lineBreak":true},{"valueType":"ALL","boundingPoly":{"vertices":[{"x":4.0,"y":32.0},{"x":81.0,"y":32.0},{"x":81.0,"y":63.0},{"x":4.0,"y":63.0}]},"inferText":"활명수","inferConfidence":0.9995,"type":"NORMAL","lineBreak":true}]}]}'
-
-ocr_json = json.loads(text)
-
-items_dict = ocr_json["images"][0]['fields']
-
-# print(items_dict)
-
-df = pd.DataFrame(items_dict)
-
-for text in df['inferText'].values:
-    if text in pill_df['itemName'].values:
-        print(pill_df[pill_df['itemName']==text])
-
+ocr.main('../memo.png', 'pill.csv')
