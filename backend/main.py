8d924cf5913905cf132901426abdbe884e56f133
gnsdp99 <rladpgns99@gmail.com>
2022-10-18 13:30:41 +0900

    _

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,8 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from ocr import clova_ocr_api as ocr
 app = FastAPI() # API 생성
 
-
+@app.get('/')
+async def do_ocr():
+    return ocr.main('../memo.png', '../ocr/pill.csv')
 # 터미널에 다음 코드 실행
 # $ uvicorn main:app --reload
