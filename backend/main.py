9fdf4d07b9e776930b423f80b232ef459da06ca6
haunJo <johaun12@naver.com>
2022-10-18 13:30:46 +0900

    jo

--- backend/main.py
+++ backend/main.py
@@ -1,59 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from pandas import array
 from pydantic import BaseModel
-import numpy as np
 
-from fastapi.encoders import jsonable_encoder #data->json으로 바꿔줌
-import httpx # 외부 API 요청 라이브러리
-import uuid
-import time
-import json
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from ocr import clova_ocr_api as ocr
 app = FastAPI() # API 생성
 
-
-
-# class Item(BaseModel): # 
-#     text: str
-pill_list = np.array
-
-pill_url = 'http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList'
-pill_params = {'serviceKey' : 'PvrIUvAktOxd6AOTm2RPSs8ovw0vx517mITghrvn8+qD/IHJZQd4j+mjnD9nrT4/arMuTl44EGSvLLym15eBRQ==','itemName' : '타이레놀', 'type' : 'json'}
-@app.get("/pill_api/")
-async def call_pill_api():
-    async with httpx.AsyncClient() as client:
-        response = await client.get(pill_url, params=pill_params) 
-        data = response.text
-        return jsonable_encoder(data)
-
-ocr_url = 'https://8vhb9to58j.apigw.ntruss.com/custom/v1/18483/a614b583ca0ed9d541b5609697d1de6cc3229dc2f7001e17ed228e6453e39dff/general'
-image_file = 'pills.jpeg' # ocr_params start
-secret_key = 'WllGYkRRb1VPTkN0Y3FhYkVLTVlMVlFFQXRXWWdjQWM='
-request_json = {
-    'images': [
-        {
-            'format': 'jpg',
-            'name': 'demo'
-        }
-    ],
-    'requestId': str(uuid.uuid4()),
-    'version': 'V2',
-    'timestamp': int(round(time.time() * 1000))
-} 
-payload = {'message': json.dumps(request_json).encode('UTF-8')}
-files = [
-  ('file', open(image_file,'rb'))
-]
-headers = {
-  'X-OCR-SECRET': secret_key
-} # ocr_params end
-@app.get("/ocr_api/")
-async def call_ocr_api():
-    async with httpx.AsyncClient() as client:
-        response = await client.get(ocr_url, headers=headers, data=payload, files=files)
-        data = response.text
-        return data
-    
-# 터미널에 다음 코드 실행
-# $ uvicorn main:app --reload
+@app.get('/')
+async def do_ocr():
+    return ocr.main('../memo.png', '../ocr/pill.csv')
--- ocr/clova_ocr_api.py
+++ ocr/clova_ocr_api.py
@@ -3,7 +3,7 @@ import uuid
 import time
 import json
 import pandas as pd
-
+  
 # APIGW Invoke URL
 api_url = 'https://8vhb9to58j.apigw.ntruss.com/custom/v1/18483/a614b583ca0ed9d541b5609697d1de6cc3229dc2f7001e17ed228e6453e39dff/general'
 
@@ -12,49 +12,53 @@ secret_key = 'WllGYkRRb1VPTkN0Y3FhYkVLTVlMVlFFQXRXWWdjQWM='
 # CLOVA OCR Invoke URL
 # http://clovaocr-api-kr.ncloud.com/external/v1/18483/a614b583ca0ed9d541b5609697d1de6cc3229dc2f7001e17ed228e6453e39dff
 
-# ocr 대상 이미지
-image_file = '../제목 없음.png'
+def call_ocr_api(image_file): # naver clova api 호출
+  request_json = {
+      'images': [
+          {
+              'format': 'jpg',
+              'name': 'demo'
+          }
+      ],
+      'requestId': str(uuid.uuid4()),
+      'version': 'V2',
+      'timestamp': int(round(time.time() * 1000))
+  }
+  payload = {'message': json.dumps(request_json).encode('UTF-8')}
+  files = [
+    ('file', open(image_file,'rb'))
+  ]
+  headers = {
+    'X-OCR-SECRET': secret_key
+  } 
+  response = requests.request("POST", api_url, headers=headers, data = payload, files = files)
+  return response.text # json file
 
-# ocr api 코드
-request_json = {
-    'images': [
-        {
-            'format': 'jpg',
-            'name': 'demo'
-        }
-    ],
-    'requestId': str(uuid.uuid4()),
-    'version': 'V2',
-    'timestamp': int(round(time.time() * 1000))
-}
+def parse_json(json_file): # ocr 결과를 필요한 정보만 파싱
+  ocr_json = json.loads(json_file) # json Object
+  ocr_array = ocr_json.get('images')[0].get('fields')
+  text_list = []
+  confidence_list = []
+  for element in ocr_array:
+    infer_text.append(element.get('inferText'))
+    infer_confidence.append(element.get('inferConfidence'))
+  infer_dict = {'inferText': text_list, 'inferConfidence': confidence_list}
+  return infer_dict
 
-payload = {'message': json.dumps(request_json).encode('UTF-8')}
-files = [
-  ('file', open(image_file,'rb'))
-]
-headers = {
-  'X-OCR-SECRET': secret_key
-}
-response = requests.request("POST", api_url, headers=headers, data = payload, files = files)
-
-# ocr 결과를 DataFrame으로 저장
-ocr_json = json.loads(response.text)
-# fields = ocr_json["images"][0]['fields']
-# df = pd.DataFrame(fields)                     
-print(ocr_json)
-# # 약 상세정보를 csv파일로 저장했을 때 실행 코드
-# pill_df = pd.read_csv('pill.csv')
-# for text in df['inferText'].values: 
-#     if text in pill_df['itemName'].values:
-#         print(pill_df[pill_df['itemName']==text])
-
-
-# url_pill = 'http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList' # 약 상세정보 api 주소
-
-# for text in df['inferText'].values:
-#   # ocr 결과 text를 약 상세정보 api 요청 파라미터로 전달
-#   params_pill ={'serviceKey' : 'PvrIUvAktOxd6AOTm2RPSs8ovw0vx517mITghrvn8+qD/IHJZQd4j+mjnD9nrT4/arMuTl44EGSvLLym15eBRQ==', 'itemName' : text, 'type' : 'json' }
-#   response = requests.get(url_pill, params=params)
-#   pill_json = json.loads(response.text)
-#   items_dict = pill_json["body"]["items"] # 리스트로 감싼 dict
+# def check_pill_db(ocr_dict, pill_db): # ocr 결과가 약 DB에 있는지 확인
+#   pill_name_list = pill_db['itemName'].values 
+#   text_list = ocr_dict['inferText']
+#   confidence_list = ocr_dict['inferConfidence']
+#   for text, confidence in zip(text_list, confidence_list):
+#     if (confidence > 0.5) and (text in pill_db): # confidence가 0.5보다 높고 pill_db에 존재하는 text만 사용자 db에 저장한다. 
+#       print(pill_db[pill_db['itemName']==text]) # 추후에 DB에 저장하는 코드로 수정해야 함.
+      
+def check_pill_db(ocr_dict):
   
+  
+def main(image_file, pill_db):
+  json_file = call_ocr_api(image_file)
+  infer_dict = parse_json(json_file)
+  check_pill_db(infer_dict, pill_db)
+  
+print(call_ocr_api('../memo.png'))
