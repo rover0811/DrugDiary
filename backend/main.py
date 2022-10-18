77a9a431e7c1ea69f6385471d8f12232c04948ee
haunJo <johaun12@naver.com>
2022-10-18 17:20:31 +0900

    데이터전처리

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import FileResponse
 
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -10,3 +11,7 @@ app = FastAPI() # API 생성
 @app.get('/')
 async def do_ocr():
     return ocr.main('../memo.png', '../ocr/pill.csv')
+
+@app.get('/pill')
+async def write_pill():
+    return FileResponse('result.json')
