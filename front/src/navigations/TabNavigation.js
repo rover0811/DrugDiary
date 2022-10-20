da2e8f03238f0de8c7b00d63415356f5681c3e49
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-10-20 12:13:28 +0900

    [feat] tabnavigation

--- front/src/navigations/TabNavigation.js
+++ front/src/navigations/TabNavigation.js
@@ -1,145 +1,110 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useNavigation } from "@react-navigation/native";
 import { MonthlyCalendar } from "../screens/MonthlyCalendar";
 // import { Home } from "../screens/Home";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 import { Calendar, Agenda } from "react-native-calendars";
+import { NavigationContainer } from "@react-navigation/native";
 
 const TabIcon = ({ name, size, color }) => {
   return <MaterialCommunityIcons name={name} size={size} color={color} />;
 };
 
 const Tab = createBottomTabNavigator();
-
 const TabNavigation = () => {
+  const navigation = useNavigation();
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name="TODO List"
-        component={Home}
-        options={{
-          headerShown: false,
-          tabBarIcon: (props) => TabIcon({ ...props, name: "calendar-check" }),
-        }}
-      />
-      <Tab.Screen
-        name="Calendar"
+        name="Home"
         component={MonthlyCalendar}
         options={{
           headerShown: false,
-          tabBarIcon: (props) => TabIcon({ ...props, name: "calendar" }),
+          tabBarIcon: (props) => TabIcon({ ...props, name: "home" }),
+          tabBarActiveTintColor: "#1B4B66",
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: (props) => TabIcon({ ...props, name: "history" }),
+          tabBarActiveTintColor: "#1B4B66",
+        }}
+      />
+      <Tab.Screen
+        name={" "}
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity onPress={() => navigation.navigate(" ")}>
+              <View
+                style={{
+                  width: 55,
+                  height: 55,
+                  backgroundColor: "#1B4B66",
+                  borderRadius: 30,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  // marginBottom: Platform.OS == "android" ? 50 : 30,
+                  marginBottom: Platform.OS == "android" ? 20 : 10,
+                }}
+              >
+                <Image
+                  source={require("../../image/plus.png")}
+                  style={{
+                    width: 22,
+                    height: 22,
+                    tintColor: "white",
+                  }}
+                ></Image>
+              </View>
+            </TouchableOpacity>
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Add"
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: (props) => TabIcon({ ...props, name: "pill" }),
+          tabBarActiveTintColor: "#1B4B66",
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarIcon: (props) => TabIcon({ ...props, name: "calendar-check" }),
+          tabBarActiveTintColor: "#1B4B66",
         }}
       />
     </Tab.Navigator>
   );
 };
-export class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: {},
-    };
-  }
 
-  render() {
-    return (
-      <Agenda
-        items={this.state.items}
-        loadItemsForMonth={this.loadItems.bind(this)}
-        // selected={"2017-05-16"}
-        renderItem={this.renderItem.bind(this)}
-        // onDayPress={(day) => {
-        //   alert("day pressed");
-        // }}
-        onCalendarToggled={(calendarOpened) => {}}
-        // renderEmptyDate={this.renderEmptyDate.bind(this)}
-        // rowHasChanged={this.rowHasChanged.bind(this)}
-        // markingType={'period'}
-        // markedDates={{
-        //    '2017-05-08': {textColor: '#666'},
-        //    '2017-05-09': {textColor: '#666'},
-        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-        //    '2017-05-21': {startingDay: true, color: 'blue'},
-        //    '2017-05-22': {endingDay: true, color: 'gray'},
-        //    '2017-05-24': {startingDay: true, color: 'gray'},
-        //    '2017-05-25': {color: 'gray'},
-        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-        // monthFormat={'yyyy'}
-        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-        renderDay={(day, item) => <Text>{day ? day.day : "item"}</Text>}
-      />
-    );
-  }
-
-  loadItems(day) {
-    setTimeout(() => {
-      for (let i = 0; i < 1; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          // const numItems = Math.floor(Math.random() * 5);
-          // for (let j = 0; j < numItems; j++) {
-          //   this.state.items[strTime].push({
-          //     name: "Item for " + strTime,
-          //     height: Math.max(50, Math.floor(Math.random() * 150)),
-          //   });
-          // }
-        }
-      }
-      //console.log(this.state.items);
-      const newItems = {};
-      Object.keys(this.state.items).forEach((key) => {
-        newItems[key] = this.state.items[key];
-      });
-      this.setState({
-        items: newItems,
-      });
-    }, 1000);
-    // console.log(`Load Items for ${day.year}-${day.month}`);
-  }
-
-  renderItem(item) {
-    return (
-      <View style={[styles.item, { height: item.height }]}>
-        <Text>{item.name}</Text>
-      </View>
-    );
-  }
-
-  renderEmptyDate() {
-    return (
-      <View style={styles.emptyDate}>
-        <Text>This is empty date!</Text>
-      </View>
-    );
-  }
-
-  rowHasChanged(r1, r2) {
-    return r1.name !== r2.name;
-  }
-
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split("T")[0];
-  }
+function EmptyScreen() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  item: {
-    backgroundColor: "white",
-    flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    marginTop: 17,
-  },
-  emptyDate: {
-    height: 15,
-    flex: 1,
-    paddingTop: 30,
-  },
-});
+function Home() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+function Account() {
+  return (
+    <View>
+      <Text>Account</Text>
+    </View>
+  );
+}
 
 export default TabNavigation;
