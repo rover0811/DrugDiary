23f2e92914e117ce654b457757e0a9a56a9df939
HaunJo <johaun12@naver.com>
2022-11-02 13:31:16 +0900

    [fix] backend

--- ocr/clova_ocr_api.py
+++ ocr/clova_ocr_api.py
@@ -47,7 +47,7 @@ def parse_json(json_file): # ocr 결과를 필요한 정보만 파싱
   ocr_array = ocr_json.get('images')[0].get('fields')
   text_list = []
   confidence_list = []
-  for element in ocr_array:
+  for element in tqdm(ocr_array):
     text_list.append(element.get('inferText'))
     confidence_list.append(element.get('inferConfidence'))
   infer_dict = {'inferText': text_list, 'inferConfidence': confidence_list}
@@ -58,14 +58,9 @@ def check_pill_db(ocr_dict, pill_db): # ocr 결과가 약 DB에 있는지 확인
   pill_name_list = pill_df['itemName'].values 
   text_list = ocr_dict['inferText']
   confidence_list = ocr_dict['inferConfidence']
-  first = 1
   for text, confidence in tqdm(zip(text_list, confidence_list)):
     if (confidence > 0.5) and (text in pill_name_list):# confidence가 0.5보다 높고 pill_db에 존재하는 text만 사용자 db에 저장한다.
-      if first:
-        df = pd.DataFrame(pill_df[pill_df['itemName']==text])
-        first = 0
-      else:
-        df = df.append(pill_df[pill_df['itemName']==text])
+      df = pd.DataFrame(pill_df[pill_df['itemName']==text])
       df.to_json('DB.json', indent = 4, force_ascii=False)
       Process_onDB()
       print(pill_df[pill_df['itemName']==text]) # 추후에 DB에 저장하는 코드로 수정해야 함.
