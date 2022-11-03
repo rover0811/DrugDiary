449a1c1e04ccb46f745a8372c46057ff6f708850
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-11-03 22:54:58 +0900

    [fix] fix calendar view

--- front/App.js
+++ front/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TabNavigation from "./navigations/TabNavigation";
+import TabNavigation from "./src/navigations/TabNavigation";
 import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
