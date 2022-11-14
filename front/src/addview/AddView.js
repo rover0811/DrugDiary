63ee307773d023358afe3c10da3c999feacd31da
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-11-15 05:56:47 +0900

    [feat] add upload image by camera

--- front/src/addview/AddView.js
+++ front/src/addview/AddView.js
@@ -1,15 +1,32 @@
-import React from "react";
+import { React, useState } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Pressable,
-  StyleSheet,
-} from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import * as ImagePicker from "expo-image-picker";
 
 export default function AddView() {
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  const [imageUrl, setImageUrl] = useState("");
+
+  const uploadImage = async () => {
+    if (!status?.granted) {
+      const permission = await requestPermission();
+      if (!permission.granted) {
+        return null;
+      }
+    }
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+      aspect: [1, 1],
+    });
+    if (result.cancelled) {
+      return null;
+    }
+    console.log(result.uri);
+    setImageUrl(result.uri);
+  };
   return (
     <View style={{ height: "100%", backgroundColor: "white", padding: 18 }}>
       <Text style={{ fontSize: 20, fontWeight: "bold" }}>최지우님,</Text>
@@ -33,24 +50,24 @@ export default function AddView() {
         </TouchableOpacity>
       </View>
       <View style={styles.buttonView}>
-        <Pressable
+        <TouchableOpacity
           style={[styles.button, styles.buttonClose, { marginBottom: 20 }]}
-          // onPress={storeAndCloseModal}
+          onPress={uploadImage}
         >
           <Text style={styles.textStyle}>촬영하여 약 저장</Text>
-        </Pressable>
-        <Pressable
+        </TouchableOpacity>
+        <TouchableOpacity
           style={[styles.button, styles.buttonClose, { marginBottom: 20 }]}
           // onPress={storeAndCloseModal}
         >
           <Text style={styles.textStyle}>약 직접 입력</Text>
-        </Pressable>
-        <Pressable
+        </TouchableOpacity>
+        <TouchableOpacity
           style={[styles.button, styles.buttonClose, { marginBottom: 20 }]}
           // onPress={storeAndCloseModal}
         >
           <Text style={styles.textStyle}>약 바구니로 가기</Text>
-        </Pressable>
+        </TouchableOpacity>
       </View>
     </View>
   );
