09c73bca22ac537a2dbe4e1879cc986110179482
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-11-17 20:49:19 +0900

    [feat] Add Chart View

--- front/src/navigations/TabNavigation.js
+++ front/src/navigations/TabNavigation.js
@@ -9,6 +9,7 @@ import PlusModal from "../modal/PlusModal";
 import AddView from "../addview/AddView";
 import { LineChart } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
+import Chart from "../chart/Chart";
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -39,7 +40,7 @@ const TabNavigation = () => {
       />
       <Tab.Screen
         name="History"
-        component={EmptyScreen}
+        component={Chart}
         options={{
           tabBarIcon: (props) => TabIcon({ ...props, name: "history" }),
           tabBarActiveTintColor: "#1B4B66",
@@ -105,50 +106,7 @@ const TabNavigation = () => {
 function EmptyScreen() {
   return (
     <View>
-      <Text>Bezier Line Chart</Text>
-      <LineChart
-        data={{
-          labels: ["January", "February", "March", "April", "May", "June"],
-          datasets: [
-            {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-              ],
-            },
-          ],
-        }}
-        width={Dimensions.get("window").width} // from react-native
-        height={220}
-        yAxisLabel="$"
-        yAxisSuffix="k"
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={{
-          backgroundColor: "#e26a00",
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#ffa726",
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
+      <Text>Hello</Text>
     </View>
   );
 }
