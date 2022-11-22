a771bfb40fe590ac2fb3274851a2afa5b7765f49
rover0811 <rover0811@hotmail.com>
2022-11-22 13:05:45 +0900

    [fix] picker-select로 인한 종속성 오류 해결

--- front/src/chart/Chart.js
+++ front/src/chart/Chart.js
@@ -3,11 +3,14 @@ import { LineChart } from "react-native-chart-kit";
 import { format } from "date-fns";
 import { useEffect, useState } from "react";
 import { getData } from "../../DB/Store";
+import { ListItem, Icon } from "@rneui/themed";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Chart() {
   const now = new Date();
-  const day1 = new Date(now.setDate(now.getDate() - 6));
-  const day2 = new Date(now.setDate(now.getDate() + 1));
+  const day1 = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 6);
+  const day2 = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 5);
   const day3 = new Date(now.setDate(now.getDate() + 1));
   const day4 = new Date(now.setDate(now.getDate() + 1));
   const day5 = new Date(now.setDate(now.getDate() + 1));
@@ -20,7 +23,65 @@ export default function Chart() {
   const [day5Data, setday5Data] = useState(0);
   const [day6Data, setday6Data] = useState(0);
 
+  const [dayDataList, setDayDataList] = useState(
+    // [...Array(7)].map(
+    //   (value, index) => new Date(now.getFullYear(), now.getMonth(), now.getDate() - (6 - index))
+    //   )
+    []
+  );
+
+  const list2 = [
+    {
+      title: "기존 기록 보기",
+      icon: "av-timer",
+    },
+    {
+      title: "약 목록 보기",
+      icon: "pill",
+    },
+  ];
+  const list3 = [
+    {
+      name: "A",
+      subtitle: "Hi",
+    },
+    {
+      name: "B",
+      subtitle: "Hi",
+    },
+    {
+      name: "C",
+      subtitle: "Hi",
+    },
+    {
+      name: "D",
+      subtitle: "Hi",
+    },
+    
+  ];
+  const [expanded, setExpanded] = useState(false)
+  const renderItem = ({ item }) => {
+    return (
+       <AccordionListItem item={item}/>
+    )
+}
+
+  // uri;
+  // const response = await fetch(uri);
+  // const blob  = await response.blob();// 이미지 올리는 부분
+
   useEffect(() => {
+    const init = async () => {
+      try {
+        const keys = await AsyncStorage.getAllKeys();
+        const result = await Promise.all(keys.map((day) => getData(day)));
+
+        setDayDataList(result);
+      } catch (e) {
+        // read key error
+      }
+    };
+    init();
     getData(format(day1, "yyyy-MM-dd")).then((res) => {
       setday1Data(res.thirdQuestion);
     });
@@ -39,11 +100,11 @@ export default function Chart() {
     getData(format(day6, "yyyy-MM-dd")).then((res) => {
       setday6Data(res.thirdQuestion);
     });
-  }, [now]);
+  }, []);
 
   return (
     <View>
-      <Text>최지우님의 기분변화 그래프</Text>
+      <Text>기분변화 그래프</Text>
       <LineChart
         data={{
           labels: [
@@ -92,6 +153,37 @@ export default function Chart() {
           borderRadius: 16,
         }}
       />
+      <View>
+        <Text style={{ fontSize: 40 }}>약 목록</Text>
+        <ListItem
+          bottomDivider
+          onPress={async () => {
+            let keys = [];
+            try {
+              keys = await AsyncStorage.getAllKeys();
+            } catch (e) {
+              // read key error
+            }
+
+            console.log(keys);
+            // example console.log result:
+            // ['@MyApp_user', '@MyApp_key']
+          }}
+        >
+          <MaterialCommunityIcons name="history" size={20} />
+         <ListItem.Content>
+            <ListItem.Title>최근 기록들 보기</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+        <ListItem bottomDivider onPress={() => alert("hello")}>
+          <MaterialCommunityIcons name="pill" size={20} />
+          <ListItem.Content>
+            <ListItem.Title>복용하고 있는 약 보기</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem> 
+      </View>
     </View>
   );
 }
