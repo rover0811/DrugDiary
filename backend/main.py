48ffa6b99017a94647d4001fdd2ee06a6acfc22e
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2022-11-23 17:01:08 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
-
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
-
+from fastapi.encoders import jsonable_encoder
 from fastapi import UploadFile, File  # image upload
+from tempfile import NamedTemporaryFile
+from typing import IO
 import uuid
 import json
 import sys
@@ -20,24 +21,20 @@ async def input_name(name: str):
     pill_list = pill.call_api(name)
     result = {}
     result["items"] = pill_list
-    result = json.dumps(result, indent=4, ensure_ascii=False)
+    result = jsonable_encoder(result)
+    return result
 
-    with open('result.json', 'w') as f:
-        f.write(result)
-    return FileResponse("result.json")
 
+async def save_file(file: IO):
+    with NamedTemporaryFile("wb", delete=False) as tempfile:
+        tempfile.write(file.read())
+        return tempfile.name
+    
 @app.post('/pill/input_image')
 async def input_image(image: UploadFile):
-    UPLOAD_DIR = "./images"
-    if not os.path.exists(UPLOAD_DIR):
-        os.makedirs(UPLOAD_DIR)
-    content = await image.read()
-    filename = f"{str(uuid.uuid4())}.jpg"
-    with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
-        fp.write(content)
-
-    PATH = os.path.join(UPLOAD_DIR, filename)
-    ocr_response = ocr.call_api(PATH)
+    path = await save_file(image.file)
+    ocr_response = ocr.call_api(path)
+    
     text_list = ocr.get_texts(ocr_response)
 
     items = []
@@ -48,8 +45,5 @@ async def input_image(image: UploadFile):
             
     result = {}
     result['items'] = items
-    result = json.dumps(result, indent=4, ensure_ascii=False)
-
-    with open('result.json', 'w') as f:
-        f.write(result)
-    return FileResponse("result.json")
+    result = jsonable_encoder(result)
+    return result
