304f8e507c12f6741a0f25b43eb6f557910cc965
rover0811 <rover0811@hotmail.com>
2022-11-28 22:25:46 +0900

    [feat] AsyncStorage setPills함수 기능 개발

--- front/DB/Store.js
+++ front/DB/Store.js
@@ -3,8 +3,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export const storeData = async (key, value) => {
   try {
     const jsonValue = JSON.stringify(value);
-    await AsyncStorage.setItem(key, jsonValue);
-    // console.log("%s %s", key, value);
+    await AsyncStorage.setItem(key, jsonValue);// 혹시 안되면 setItem으로 바꾼다.
+    console.log("%s", jsonValue);
   } catch (e) {
     // saving error
   }
@@ -23,3 +23,31 @@ export const getData = async (key) => {
   }
   console.log("Done");
 };
+
+export const setPills =async (pills)=>{
+  try {
+    const value = getData('pillsList');// 여기서 pills 목록을 불러옴
+    // value.push(pills)
+    // pills.map((name) => itemName=name.itemName)
+    value.push(...pills)
+    storeData('pillsList',value)
+
+  } catch (e) {
+    console.log("setPills err");
+  }
+  console.log("Done");
+}
+
+export const getAllKeys = async () => { //Asyncstorage의 key들의 배열을 리턴
+  let keys = []
+  try {
+    keys = await AsyncStorage.getAllKeys()
+    console.log(keys)
+    return keys
+  } catch(e) {
+
+  }
+
+  // example console.log result:
+  // ['@MyApp_user', '@MyApp_key']
+}
