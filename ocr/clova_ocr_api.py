320ec48ebb0d8fb8fd906bae7d7f11a7ba2b4a2f
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2023-07-02 17:10:34 +0900

    Update clova_ocr_api.py

--- ocr/clova_ocr_api.py
+++ ocr/clova_ocr_api.py
@@ -11,19 +11,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-# APIGW Invoke URL (yehun)
-# api_url = 'https://8vhb9to58j.apigw.ntruss.com/custom/v1/18483/a614b583ca0ed9d541b5609697d1de6cc3229dc2f7001e17ed228e6453e39dff/general'
-# Secret Key
-# secret_key = 'WllGYkRRb1VPTkN0Y3FhYkVLTVlMVlFFQXRXWWdjQWM='
-
-# APIGW Invoke URL (haun)
-api_url = 'https://htacxyxwab.apigw.ntruss.com/custom/v1/19265/3afa64b3fd5507804c03d8feb7f9cdd58d892e02857b9d55f392552c8e63c090/general' # haun
-# Secret Key
-secret_key = 'a0VwQWt2SHNGV0F6SmFreFpjcG9UWmNuWWRVS3RPank='
-
-# CLOVA OCR Invoke URL
-# http://clovaocr-api-kr.ncloud.com/external/v1/18483/a614b583ca0ed9d541b5609697d1de6cc3229dc2f7001e17ed228e6453e39dff
-
 def call_api(image_file):
   print("OCR start")# naver clova api 호출
   request_json = {
